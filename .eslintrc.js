16db1e9fcac3e98fb6841188e43a2b8df2150fc4
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-07-02 14:29:42 +0900

    add: set up prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,4 @@
 // vercel/next.js/packages/eslint-config-next/index.jsから設定ファイルをコピーしてきて、extendsを変更します。
 // fetch や lint　結果のキャッシュが `./next` に残って開発の妨げになる可能性があるので npm script に `rm -rf ./.next` を追加
 /** @type {import('eslint').ESLint.ConfigData} */
-module.exports = { extends: "next/core-web-vitals" };
+module.exports = { extends: ["next/core-web-vitals", "prettier"] };
