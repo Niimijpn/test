b113346e6ea26a98198af6227ccf60b4194b72e0
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-07-02 15:01:10 +0900

    add: setup husky config

---
+++ .lintstagedrc.js
@@ -0,0 +1,21 @@
+// {.ts,.tsx} ファイルに対して next lint と prettier をコミット時に実行
+// https://nextjs.org/docs/pages/building-your-application/configuring/eslint#lint-staged
+
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+module.exports = {
+  "*.{ts,tsx}": [
+    // --incremental: 前回のビルドから変更されたファイルのみをコンパイルし、ビルド速度を大幅に向上させる
+    // ルートディレクトリにキャッシュ用の tsconfig.tsbuildinfo ファイルが出来てしまう
+    // --noEmit: コンパイラにコードの型チェックを実行するように指示
+    // ファイルの出力は行わない
+    () => "tsc --ncremental false --noEmit",
+    buildEslintCommand,
+    "prettier --write",
+  ],
+};
