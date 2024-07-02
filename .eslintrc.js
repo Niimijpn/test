dd692f6d8a155c41203d2fd284893ba1987ef7d3
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-07-02 14:25:49 +0900

    add: set up lint config

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+// .eslintrcファイルは扱いやすくするために拡張子を.jsにして以下のように変更します。
+// vercel/next.js/packages/eslint-config-next/index.jsから設定ファイルをコピーしてきて、extendsを変更します。
+// fetch や lint　結果のキャッシュが `./next` に残って開発の妨げになる可能性があるので npm script に `rm -rf ./.next` を追加
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = { extends: "next/core-web-vitals" };
